6b7b52645a2e8d46083ee443bbe17fd7adcae0ca
awwa93 <adam_hedkvist@hotmail.com>
2017-12-10 15:25:54 +0100

    Added presentation plots

--- macros/job301.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-OUTPUTDIR=../../data_new/
-nEvents=1000
-iJob=301
-root -q -b generateMomData.C"($iJob,\"$OUTPUTDIR\")"
-root -q -b generateTSMomData.C"($iJob,\"$OUTPUTDIR\")"
-root -q -b generatePatData.C"($iJob,\"$OUTPUTDIR\")"
--- macros/job323.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-OUTPUTDIR=../../data_new/
-nEvents=1000
-iJob=323
-root -q -b generateMomData.C"($iJob,\"$OUTPUTDIR\")"
-root -q -b generateTSMomData.C"($iJob,\"$OUTPUTDIR\")"
-root -q -b generatePatData.C"($iJob,\"$OUTPUTDIR\")"
